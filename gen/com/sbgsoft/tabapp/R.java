bfd8241efa05f39810787807508af454d846d7f7
SamIAm <SamIAm@SamIAm-PC>
2012-08-27 13:04:49 -0500

    Added font size controller

--- gen/com/sbgsoft/tabapp/R.java
+++ gen/com/sbgsoft/tabapp/R.java
@@ -15,26 +15,29 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int empty_sets=0x7f070006;
-        public static final int empty_songs=0x7f070009;
-        public static final int menu_sets=0x7f07000b;
-        public static final int menu_sets_clear=0x7f07000e;
-        public static final int menu_sets_create=0x7f07000c;
-        public static final int menu_sets_find=0x7f07000d;
-        public static final int menu_settings=0x7f07000a;
-        public static final int menu_songs=0x7f07000f;
-        public static final int menu_songs_clear=0x7f070013;
-        public static final int menu_songs_create=0x7f070010;
-        public static final int menu_songs_find=0x7f070012;
-        public static final int menu_songs_import=0x7f070011;
+        public static final int empty_sets=0x7f070009;
+        public static final int empty_songs=0x7f07000c;
+        public static final int imageButton1=0x7f070002;
+        public static final int imageButton2=0x7f070004;
+        public static final int menu_sets=0x7f07000e;
+        public static final int menu_sets_clear=0x7f070011;
+        public static final int menu_sets_create=0x7f07000f;
+        public static final int menu_sets_find=0x7f070010;
+        public static final int menu_settings=0x7f07000d;
+        public static final int menu_songs=0x7f070012;
+        public static final int menu_songs_clear=0x7f070016;
+        public static final int menu_songs_create=0x7f070013;
+        public static final int menu_songs_find=0x7f070015;
+        public static final int menu_songs_import=0x7f070014;
         public static final int pager=0x7f070000;
-        public static final int sets_list=0x7f070005;
-        public static final int sets_row_text=0x7f070002;
+        public static final int sets_list=0x7f070008;
+        public static final int sets_row_text=0x7f070005;
         public static final int song_text=0x7f070001;
-        public static final int songs_list=0x7f070008;
-        public static final int songs_row_text=0x7f070003;
-        public static final int tabSets=0x7f070004;
-        public static final int tabSongs=0x7f070007;
+        public static final int songs_list=0x7f07000b;
+        public static final int songs_row_text=0x7f070006;
+        public static final int tabSets=0x7f070007;
+        public static final int tabSongs=0x7f07000a;
+        public static final int textView1=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -45,12 +48,14 @@ public final class R {
         public static final int tab_songs=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_song=0x7f060000;
-        public static final int menu_main=0x7f060001;
+        public static final int menu_main=0x7f060000;
+        public static final int view_song_menu=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int down_arrow=0x7f040011;
         public static final int empty_song=0x7f04000e;
+        public static final int font_arrows=0x7f04000f;
         public static final int menu_sets=0x7f040004;
         public static final int menu_sets_clear=0x7f04000b;
         public static final int menu_sets_create=0x7f040005;
@@ -64,6 +69,7 @@ public final class R {
         public static final int no_sets=0x7f040002;
         public static final int no_songs=0x7f040003;
         public static final int title_activity_song=0x7f04000d;
+        public static final int up_arrow=0x7f040010;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
