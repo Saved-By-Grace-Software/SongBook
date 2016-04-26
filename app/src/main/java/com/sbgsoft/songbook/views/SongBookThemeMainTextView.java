76ec0ad9d3f78923b474df37fbdcc99c57f0191e
sfahnestock <sfahnestock@localhost>
2016-04-26 14:55:06 -0500

    Custom text view with shadow capability

--- app/src/main/java/com/sbgsoft/songbook/views/SongBookThemeMainTextView.java
+++ app/src/main/java/com/sbgsoft/songbook/views/SongBookThemeMainTextView.java
@@ -1,41 +1,49 @@
 package com.sbgsoft.songbook.views;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import com.sbgsoft.songbook.R;
 import com.sbgsoft.songbook.main.SongBookTheme;
 
 public class SongBookThemeMainTextView extends TextView {
+    private static final float SHADOW_RADIUS = 1.5f;
+    private static final int SHADOW_DX = 3;
+    private static final int SHADOW_DY = 3;
+
     public SongBookThemeMainTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init(attrs);
+        init(context, attrs);
     }
 
     public SongBookThemeMainTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(attrs);
+        init(context, attrs);
 
     }
 
     public SongBookThemeMainTextView(Context context) {
         super(context);
-        init(null);
+        init(context, null);
     }
 
-    private void init(AttributeSet attrs) {
+    private void init(Context context, AttributeSet attrs) {
         // TODO: Get current theme from database
         SongBookTheme songBookTheme = new SongBookTheme();
 
         // Set the color to the current theme color
         setTextColor(songBookTheme.getMainFontColor());
 
-        if (attrs != null) {
-            float shadowRad = attrs.getAttributeFloatValue(android.R.attr.shadowRadius, 0.0f);
+        // Get the use shadow field
+        if (context != null && attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SongBookThemeMainTextView, 0, 0);
+            boolean useShadow = a.getBoolean(R.styleable.SongBookThemeMainTextView_useShadow, false);
 
-            if (shadowRad > 0) {
+            if (useShadow) {
                 // Set shadow color
-                setShadowLayer(1.5f, 3, 3, songBookTheme.getMainFontShadowColor());
+                setShadowLayer(SHADOW_RADIUS, SHADOW_DX, SHADOW_DY, songBookTheme.getMainFontShadowColor());
             }
         }
     }
